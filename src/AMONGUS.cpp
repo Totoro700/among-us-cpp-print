d9ff4621c54d2f3cc0e323ea3cbe845f8ddf8577
Totoro700 <ianfrancisko@gmail.com>
2023-05-06 11:23:09 -0500

    src files

---
+++ src/AMONGUS.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include "amongus.h"
+
+class amongus {
+public:
+    void print(std::string a) {
+        int ascChar;
+        for (int i = 0; i < a.length(); i++)
+        {
+            ascChar = a[i];
+            int b = 0;
+            while (b < ascChar) {
+                b++;
+                RED SUS
+            }
+            BLUE SUS
+                GREEN SUS
+                PINK SUS
+        }
+    }
+
+    void printLn(std::string b) {
+        this->print(b);
+        int asd = 0;
+        while (asd < 10) {
+            RED SUS
+                asd++;
+        }
+        BLUE SUS
+            GREEN SUS
+            PINK SUS
+    }
+};
+
+
+
+
+/*
+print(string)
+uses among us language to print string
+
+printLn(string)
+uses among us language to print string with \n (line break)
+*/
+
+int main()
+{
+    amongus amongus;
+    amongus.printLn("Type message here");
+}
